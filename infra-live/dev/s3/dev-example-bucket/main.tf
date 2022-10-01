272950fa7c7e589362dec5e52d3dd13b39803394
cwp-alerts2 <103568855+cwp-alerts2@users.noreply.github.com>
2022-10-01 13:31:08 +0530

    Update main.tf

--- infra-live/dev/s3/dev-example-bucket/main.tf
+++ infra-live/dev/s3/dev-example-bucket/main.tf
@@ -2,3 +2,12 @@ module "s3-bucket_example_complete" {
   source  = "terraform-aws-modules/s3-bucket/aws//examples/complete"
   version = "3.4.0"
 }
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket here update"
+    Environment = "Dev"
+  }
+}
